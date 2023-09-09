02fa9afa9a6c5d881a15f5fd42813fd01e378d65
jeweler23 <lopatin.lopatin2011@gmail.com>
2023-09-09 14:55:45 +0300

    fixed-overflow

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ popupBtn.addEventListener('click',(e) => {
 // при нажатии на крестик класс удаляем
 popupClose.addEventListener('click',function(event){
     popupBg.classList.remove('popup-bg__active')
+    document.body.style.overflowY = 'auto'
 })
 
 //удаляем класс актив при нажатии на область вне дива попап
 document.addEventListener('click', function(event){
     if (event.target===popupBg){
         popupBg.classList.remove('popup-bg__active')
+        document.body.style.overflowY = 'auto'
     }
 })
