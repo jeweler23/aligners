3c2c9e3b661c2cba74776d2d3c83e5adc7d6a938
jeweler23 <lopatin.lopatin2011@gmail.com>
2023-09-23 23:03:03 +0300

    add accordion maket

--- app.js
+++ app.js
@@ -58,3 +58,34 @@ popupBtnCall.addEventListener('click', function(event){
     popupBgCall.classList.add('popup-bg-call__active')
     document.body.style.overflowY = "hidden";
 })
+
+
+
+//initialization accordion
+
+
+const accordionContent = document.querySelectorAll('.accordion-content')
+
+accordionContent.forEach((item,index) =>{
+    let header = item.querySelector('h2')
+    header.addEventListener('click',()=>{
+        item.classList.toggle('open');
+        let description = item.querySelector('.descriprion')
+        if(item.classList.contains('open')){
+            description.style.height =`${description.scrollHeight}px`
+            item.querySelector('i').classList.replace("fa-plus","fa-minus")
+        }
+        removeOpen(index);
+    })
+})
+
+function removeOpen(index1){
+    accordionContent.forEach((item2,index2) =>{
+        if(index1 != index2){
+            item2.classList.remove('open')
+            let description = item2.querySelector('.descriprion')
+            description.style.height = '0px'
+            item2.querySelector('i').classList.replace("fa-minus","fa-plus")
+        }
+    })
+}
