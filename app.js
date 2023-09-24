57c2e002bd7d09110acfd7431e4cb752c58a3efa
jeweler23 <lopatin.lopatin2011@gmail.com>
2023-09-24 20:22:17 +0300

    add accordion down table

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let popupClose = document.querySelectorAll('.popup__image-close')
 let popupBtnCall = document.querySelector('.popup-btn__call')
 let popupBgCall =document.querySelector('.popup-bg-call')
 
-console.log(popupClose)
+// console.log(popupClose)
 // добавляем слушателя к кнопке и класс актив диву popupBgh
 
 // document.body.addEventListener('click',(e)=>{
@@ -65,27 +65,47 @@ popupBtnCall.addEventListener('click', function(event){
 
 
 const accordionContent = document.querySelectorAll('.accordion-content')
-
+console.log(accordionContent)
 accordionContent.forEach((item,index) =>{
-    let header = item.querySelector('h2')
-    header.addEventListener('click',()=>{
-        item.classList.toggle('open');
-        let description = item.querySelector('.descriprion')
-        if(item.classList.contains('open')){
-            description.style.height =`${description.scrollHeight}px`
-            item.querySelector('i').classList.replace("fa-plus","fa-minus")
-        }
-        removeOpen(index);
+
+    let header = item.querySelectorAll('.test-accordion')
+    
+    // console.log(accordionContent)
+    header.forEach((item1,index1) =>{
+        item1.addEventListener('click',()=>{
+            console.log(item1)
+            item1.classList.toggle('open')
+            let text = item.querySelector('.test-accordion-text')
+            let image = document.querySelectorAll('.accordion-image')
+            let accordionHidden = document.querySelectorAll('.accordion-hidden') // почему не могу получить из массива accordionContent
+
+            if(item1.classList.contains('open')){
+
+                item1.style.background ='#474A6A'
+                text.style.color = '#FFF'
+                image[index].style.transform = "rotate(90deg)";
+                accordionHidden[index].style.display = 'block'
+                
+            }
+            else{
+                // item1.style.height = '0px'
+                item1.style.background ='#FFF9F4'
+                text.style.color = '#20305B'
+                image[index].style.transform = "rotate(0deg)";
+                accordionHidden[index].style.display = 'none'
+            }
+
+        })
     })
 })
 
-function removeOpen(index1){
-    accordionContent.forEach((item2,index2) =>{
-        if(index1 != index2){
-            item2.classList.remove('open')
-            let description = item2.querySelector('.descriprion')
-            description.style.height = '0px'
-            item2.querySelector('i').classList.replace("fa-minus","fa-plus")
-        }
-    })
-}
+// function removeOpen(index1){
+//     accordionContent.forEach((item2,index2) =>{
+//         if(index1 != index2){
+//             item2.classList.remove('open')
+//             let description = item2.querySelector('.descriprion')
+//             description.style.height = '0px'
+//             item2.querySelector('i').classList.replace("fa-minus","fa-plus")
+//         }
+//     })
+// }
